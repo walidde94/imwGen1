c0a3551b59b4c475b351b5671b05ae9e98b7c830
unknown <walid.de94@gmail.com>
2024-11-10 12:10:43 +0100

    init

--- my-fullstack-app/src/components/AboutMe.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AboutMe = () => {
-  return <h2>About Me Page</h2>;
-};
-
-export default AboutMe;
--- my-fullstack-app/src/components/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-  return <h2>Welcome to the Home Page</h2>;
-};
-
-export default Home;
